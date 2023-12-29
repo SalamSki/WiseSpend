56a7f6c3f08ca80667b8457e74ca9c63068c84f9
SalamSki <slome.lord123@gmail.com>
2023-12-29 17:31:33 +0100

    navbar to bottom on mobile

--- app/main/layout.tsx
+++ app/main/layout.tsx
@@ -10,7 +10,7 @@ export default async function Layout({
   const user = await getUserByID({ id: true, isVerified: true });
   const verified = !!user?.isVerified;
   return (
-    <div className="flex h-screen w-full flex-col md:flex-row">
+    <div className="flex h-screen w-full flex-col-reverse md:flex-row">
       <SideNav />
       <main className="flex max-h-screen grow flex-col items-center overflow-y-auto px-4 md:py-12">
         {verified ? children : <VerifyPage user_id={user?.id} />}
